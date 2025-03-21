558498a7f40b8c73b7bf8ceb718b8dd01d8edc5e
2003monica <monicapriyam@gmail.com>
2025-03-21 12:22:48 -0400

    Updated app.py with new changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -24,4 +25,6 @@ def webhook():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, port=port, host="0.0.0.0")
+
